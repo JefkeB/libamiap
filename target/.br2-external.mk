83be475218651db539d43c79ee068e549a1bfcb5
JefkeB <jefkeb@bergenv.nl>
2020-02-27 10:03:31 +0100

    after first make run, changed default value for our custom parameter

---
+++ target/.br2-external.mk
@@ -0,0 +1,14 @@
+#
+# Automatically generated file; DO NOT EDIT.
+#
+
+BR2_EXTERNAL ?= /home/jef/target
+BR2_EXTERNAL_NAMES = 
+BR2_EXTERNAL_DIRS = 
+BR2_EXTERNAL_MKS = 
+
+BR2_EXTERNAL_NAMES += MAP
+BR2_EXTERNAL_DIRS += /home/jef/target
+BR2_EXTERNAL_MKS += /home/jef/target/external.mk
+export BR2_EXTERNAL_MAP_PATH = /home/jef/target
+export BR2_EXTERNAL_MAP_DESC = Minimal audio player
