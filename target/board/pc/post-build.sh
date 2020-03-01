5c8f5d79e4d7b51dfbd1d90a89d869f3b87c7a49
JefkeB <jefkeb@bergenv.nl>
2020-03-01 09:38:55 +0100

    changed files for our board

--- target/board/pc/post-build.sh
+++ target/board/pc/post-build.sh
@@ -4,7 +4,14 @@ set -e
 
 BOARD_DIR=$(dirname "$0")
 
+echo "$BOARD_DIR"
+echo "$TARGET_DIR"
+echo "$HOST_DIR"
+echo "$BINARIES_DIR"
 cp -f "$BOARD_DIR/grub-bios.cfg" "$TARGET_DIR/boot/grub/grub.cfg"
 
 # Copy grub 1st stage to binaries, required for genimage
 cp -f "$HOST_DIR/lib/grub/i386-pc/boot.img" "$BINARIES_DIR"
+
+
+echo "***********************************************************************"
